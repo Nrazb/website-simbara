aad62359fe00da5e375af970de6b568c782d2848
Nrazb <nauraazzahra457@gmail.com>
2025-10-04 16:40:56 +0700

    fix tailwind bro
    dah jalan tailwind nya boss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,6 @@ module.exports = {
     './resources/**/*.blade.php',
     './resources/**/*.js',
     './resources/**/*.vue',
-    './storage/framework/views/*.php',
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
   ],
   theme: {
     extend: {
