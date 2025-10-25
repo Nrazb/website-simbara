96ce10be5c32c252122badabcc0adc47962f4228
IqroNegoro <iqronegoro0@gmail.com>
2025-10-25 21:24:52 +0700

    feat: setup api docs

--- routes/api.php
+++ routes/api.php
@@ -10,14 +10,16 @@ use App\Http\Controllers\Api\MaintenanceItemRequestApiController;
 use App\Http\Controllers\Api\RemoveItemRequestApiController;
 use App\Http\Controllers\AuthController;
 
-Route::post('/login', [AuthController::class, 'apiLogin']);
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::resource('users', UserApiController::class);
-    Route::resource('item-requests', ItemRequestApiController::class);
-    Route::resource('types', TypeApiController::class);
-    Route::resource('items', ItemApiController::class);
-    Route::resource('mutation-item-requests', MutationItemRequestApiController::class);
-    Route::resource('maintenance-item-requests', MaintenanceItemRequestApiController::class);
-    Route::resource('remove-item-requests', RemoveItemRequestApiController::class);
+Route::name("api.")->group(function() {
+    Route::post('/login', [AuthController::class, 'apiLogin']);
+    
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::resource('users', UserApiController::class);
+        Route::resource('item-requests', ItemRequestApiController::class);
+        Route::resource('types', TypeApiController::class);
+        Route::resource('items', ItemApiController::class);
+        Route::resource('mutation-item-requests', MutationItemRequestApiController::class);
+        Route::resource('maintenance-item-requests', MaintenanceItemRequestApiController::class);
+        Route::resource('remove-item-requests', RemoveItemRequestApiController::class);
+    });
 });
